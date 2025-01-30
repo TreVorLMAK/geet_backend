d67a811583c0a03bcf764824e1876f41d1206dc5
TreVorLMAK <114142927+TreVorLMAK@users.noreply.github.com>
2025-01-30 20:48:51 +0545

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -12,7 +12,7 @@ router.get('/profile', authMiddleware, async (req, res) => {
       return res.status(401).json({ status: "error", message: "Unauthorized access" });
     }
 
-    const user = await User.findById(req.user.id).select("-password").lean();
+    const user = await User.findById(req.user.userId).select("-password").lean();
     if (!user) {
       return res.status(404).json({ status: "error", message: "User not found" });
     }
